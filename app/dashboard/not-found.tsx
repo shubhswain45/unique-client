e06dbaaebcc427a6bc2a09847585a027448c1c9f
shubhswain45 <shubhkantswain@.com>
2024-11-24 22:38:12 +0530

    kkk

--- app/dashboard/not-found.tsx
+++ app/dashboard/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
     <div className="flex h-full flex-col items-center justify-center gap-2">
       <Frown className="w-10 text-gray-400 mt-10"/> {/* Added mt-4 here for spacing */}
       <h2 className="text-xl font-semibold">404 Not Found</h2> {/* Added mt-4 here */}
-      <p className="mt-2">Sorry, User Doesn't exist</p> {/* Added mt-2 here */}
+      <p className="mt-2">Sorry, User Doesnt exist</p> {/* Added mt-2 here */}
     </div>
   );
 }
